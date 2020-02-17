5170d70ab9232cd0c0a59324ca29d9f87d438488
FredrikMeisingseth <fredrik@LAPTOP-ACCOH9OE.localdomain>
2020-02-17 09:25:10 +0100

    structurally done with gpu generation

--- DeepTrack 1.0/DT_extension_dev.py
+++ DeepTrack 1.0/DT_extension_dev.py
@@ -114,9 +114,21 @@ def get_image_generator(image_parameters_function=lambda : get_image_parameters(
     while image_number<max_number_of_images:
         
         image_parameters = image_parameters_function()
+
+        import time
+        t0 = time.time()
+        
         image = generate_image_with_gpu(image_parameters)
 
-        yield image_number, image, image_parameters
+        t1 = time.time()
+        print("calctime with gpu", t1-t0)
+
+        image2 = generate_image(image_parameters)
+
+        t2 = time.time()
+        print("calctime without gpu", t2-t1)
+
+        yield image_number, image,image2, image_parameters
         image_number += 1
 
 def generate_image_with_gpu(image_parameters):
@@ -146,17 +158,135 @@ def generate_image_with_gpu(image_parameters):
 
 
     from numpy import meshgrid, arange, ones, zeros, sin, cos, sqrt, clip, array
+    from numpy.random import poisson as poisson
+    
+    particle_center_x_list = image_parameters['Particle Center X List']
+    particle_center_y_list = image_parameters['Particle Center Y List']
+    particle_radius_list = image_parameters['Particle Radius List']
+    particle_bessel_orders_list = image_parameters['Particle Bessel Orders List']
+    particle_intensities_list = image_parameters['Particle Intensities List']
+    image_half_size = image_parameters['Image Half-Size'] 
+    image_background_level = image_parameters['Image Background Level']
+    signal_to_noise_ratio = image_parameters['Signal to Noise Ratio']
+    gradient_intensity = image_parameters['Gradient Intensity']
+    gradient_direction = image_parameters['Gradient Direction']
+    ellipsoidal_orientation_list = image_parameters['Ellipsoid Orientation']
+    ellipticity = image_parameters['Ellipticity']
+    
+
+    ### CALCULATE IMAGE PARAMETERS
+    # calculate image full size
+    image_size = image_half_size * 2 + 1    
+    
+    ### CALCULATE BACKGROUND
+    # initialize the image at the background level
+    image_background = ones((image_size, image_size)) * image_background_level
+
+    # calculate matrix coordinates from the center of the image
+    image_coordinate_x, image_coordinate_y = meshgrid(arange(-image_half_size, image_half_size + 1), 
+                                                      arange(-image_half_size, image_half_size + 1), 
+                                                      sparse=False, 
+                                                      indexing='ij')
+    
+    # add gradient to image background
+    if gradient_intensity!=0:
+        image_background = image_background + gradient_intensity * (image_coordinate_x * sin(gradient_direction) + 
+                                                                    image_coordinate_y * cos(gradient_direction) ) / (sqrt(2) * image_size)
+
+    ### CALCULATE IMAGE PARTICLES
+    image_particles = zeros((image_size, image_size))
+
+
+    # shift particle centers so that the origin is in top left corner of image instead of the center. 
+    #TODO: this solution is not optimal, work in same coordinate system from the beginning instead.
+    particle_center_x_list = particle_center_x_list + image_half_size
+    particle_center_y_list = particle_center_y_list + image_half_size
+
+    # calculate the particle profiles of all particles and add them to image_particles
+    calc_particle_profile(particle_center_x_list, particle_center_y_list,particle_radius_list, image_particles,particle_intensities_list)
+    
+
+    # calculate image without noise as background image plus particle image
+    image_particles_without_noise = clip(image_background + image_particles, 0, 1)
+
+    ### ADD NOISE
+    image_particles_with_noise = poisson(image_particles_without_noise * signal_to_noise_ratio**2) / signal_to_noise_ratio**2
+    
+
+    return image_particles_with_noise
+
+def calc_particle_profile(particle_center_x_list, particle_center_y_list,particle_radius_list, image_particles,particle_intensities_list):
+    from numba import cuda
+    from math import ceil,exp
+
+
+    # the cuda kernel calculating the value of the Gauss function for each pixel in out image
+    @cuda.jit
+    def part_prof(d_dist_x,d_dist_y,d_radiuses,d_img_part,d_particle_intensities):
+
+        x, y = cuda.grid(2)
+
+        if x >= d_img_part.shape[0] and y >= d_img_part.shape[1]:
+            # Quit if (x, y) is outside of valid C boundary
+            return
+
+        for i in range(d_dist_x.shape[0]):
+
+            tmp = d_particle_intensities[i][0][0]*exp(-((x-d_dist_x[i])**2/(2*d_radiuses[i]**2) + (y-d_dist_y[i])**2/(2*d_radiuses[i]**2)))
+
+            d_img_part[x, y] = d_img_part[x,y] + tmp
+
+    # define threads per block and blocks per grid. This dictates how our cuda kernel devides tasks.
+    TPB = 32
+    threadsperblock = (TPB, TPB)
+    blockspergrid_x = int(ceil(image_particles.shape[0] / threadsperblock[0]))
+    blockspergrid_y = int(ceil(image_particles.shape[1] / threadsperblock[1]))
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
+    
+    # introduce stream dictating the order of data being sent to GPU
+    # create an cuda object of each object we wish to have handled by the GPU. This is because data transfer to and from GPU is costly.
+    stream = cuda.stream()
+    d_pos_x = cuda.to_device(particle_center_x_list,stream = stream)
+    d_pos_y = cuda.to_device(particle_center_y_list,stream = stream)
+    d_radiuses = cuda.to_device(particle_radius_list,stream = stream)
+    d_img_part = cuda.to_device(image_particles,stream = stream)
+    d_particle_intensities = cuda.to_device(particle_intensities_list,stream = stream)
+
+    # call the cuda kernel
+    part_prof[blockspergrid, threadsperblock](d_pos_x,d_pos_y,d_radiuses, d_img_part,d_particle_intensities)
+
+    # retrieve our image particle matrix from GPU
+    d_img_part.copy_to_host(image_particles, stream = stream)
+
+def generate_image(image_parameters):
+    
+    """Generate image with particles.
+    
+    Input:
+    image_parameters: list with the values of the image parameters in a dictionary:
+        image_parameters['Particle Center X List']
+        image_parameters['Particle Center Y List']
+        image_parameters['Particle Radius List']
+        image_parameters['Particle Bessel Orders List']
+        image_parameters['Particle Intensities List']
+        image_parameters['Image Half-Size']
+        image_parameters['Image Background Level']
+        image_parameters['Signal to Noise Ratio']
+        image_parameters['Gradient Intensity']
+        image_parameters['Gradient Direction']
+        image_parameters['Ellipsoid Orientation']
+        image_parameters['Ellipticity']
+        
+    Note: image_parameters is typically obained from the function get_image_parameters()
+        
+    Output:
+    image: image of the particle [2D numpy array of real numbers betwen 0 and 1]
+    """
+
+
+    from numpy import meshgrid, arange, ones, zeros, sin, cos, sqrt, clip, array
     from scipy.special import jv as bessel
     from numpy.random import poisson as poisson
-
-    from numba import jit, cuda
-
-    @jit(target = "cuda")
-    def part_prof(bessel_order,intensity):
-        image_particle = 4 * particle_bessel_order**2.5 * (bessel(particle_bessel_order, elliptical_distance_from_particle) / elliptical_distance_from_particle)**2
-        image_particles = image_particles + particle_intensity * image_particle
-
-
     
     particle_center_x_list = image_parameters['Particle Center X List']
     particle_center_y_list = image_parameters['Particle Center Y List']
@@ -220,10 +350,10 @@ def generate_image_with_gpu(image_parameters):
 
         # calculate particle profile
         for particle_bessel_order, particle_intensity in zip(particle_bessel_orders, particle_intensities):
-            part_prof(particle_bessel_order, particle_intensity)
+            image_particle = 4 * particle_bessel_order**2.5 * (bessel(particle_bessel_order, elliptical_distance_from_particle) / elliptical_distance_from_particle)**2
+            image_particles = image_particles + particle_intensity * image_particle
 
-    
-    
+        
 
     # calculate image without noise as background image plus particle image
     image_particles_without_noise = clip(image_background + image_particles, 0, 1)
@@ -234,3 +364,5 @@ def generate_image_with_gpu(image_parameters):
 
     return image_particles_with_noise
 
+    
+   
